b183aff2f4ba07f031b92a3013a36b244734d09b
jrbigmon <juniorterriaga0@gmail.com>
2023-10-09 20:05:36 -0300

    feat: created first method to post a new user and inject class

--- src/main/java/br/com/vagnersiqueira/todolist/controller/UserController.java
+++
@@ -1,8 +0,0 @@
-package br.com.vagnersiqueira.todolist.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class UserController {
-
-}
---
+++ src/main/java/br/com/vagnersiqueira/todolist/users/UserModel.java
@@ -0,0 +1,19 @@
+package br.com.vagnersiqueira.todolist.users;
+
+public class UserModel {
+  private String username;
+  private String name;
+  private String password;
+
+  public String getName() {
+    return name;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+}
